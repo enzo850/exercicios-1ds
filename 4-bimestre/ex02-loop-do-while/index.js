5f7ed4d64d8d7ceb0d8061b2e59b925230d7d27c
Enzofelixx <francisco.felix144@aluno.ce.gov.br>
2025-10-14 13:42:05 +0000

    feito!!!

--- 4-bimestre/ex02-loop-do-while/index.js
+++ 4-bimestre/ex02-loop-do-while/index.js
@@ -1,4 +1,8 @@
 // CRIE UMA LÓGICA AQUI
-
+let resultado = 5
+do{
+    console.log(`contando ${resultado}`)
+    resultado++
+}while(resultado <10  )
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
